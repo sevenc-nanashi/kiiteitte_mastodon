dfac1edadc82e276e2215fc4c802a9093d704c93
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-02-09 06:35:57 +0900

    Change: cmd/mainに

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/sevenc-nanashi/kiiteitte_mastodon/cmd"
+	cmd "github.com/sevenc-nanashi/kiiteitte_mastodon/cmd/main"
 
 	"github.com/joho/godotenv"
 	colorable "github.com/mattn/go-colorable"
